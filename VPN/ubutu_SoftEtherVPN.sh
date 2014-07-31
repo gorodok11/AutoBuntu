908ab1ad89703e5038d924581eda3c6f4bb76bdf
gorodok11 <UnitLinux>
2014-07-31 12:48:03 +0400

    VPN

--- VPN/ubutu_SoftEtherVPN.sh
+++ VPN/ubutu_SoftEtherVPN.sh
@@ -14,4 +14,15 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-apt-get -y install libreadline-dev libssl-dev libncurses5-dev zlib1g-dev
+apt-get -y install build-essential libreadline-dev libssl-dev libncurses5-dev zlib1g-dev make checkinstall
+
+mkdir -p /opt/install
+cd /opt/install
+# Вместо «make install» я пишу «checkinstall», дабы менеджер пакетов apt знал о новой программулине и мог потом ее корректно удалить (подробнее здесь).
+git clone https://github.com/unitlinux/SoftEtherVPN.git
+cd SoftEtherVPN
+./configure
+make
+checkinstall
+
+vpnserver start
